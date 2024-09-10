6324ed53833e6ffec4cd5d7cab014d686608355c
fahmifath <fahmifthur@gmail.com>
2024-09-10 08:10:22 +0700

    1

--- jobsheet-1/2-encapsulation.php
+++ jobsheet-1/2-encapsulation.php
@@ -1,6 +1,7 @@
 <?php
 //membuat kelas mahasiswa
-class Mahasiswa {
+class Mahasiswa
+{
     //membuat atribut/properti nama, nim, dan jurusan pd class mahasiswa menjadi private, 
     //artinya hanya bisa diakses oleh class yg membuatnya
     private $nama;
@@ -8,41 +9,49 @@ class Mahasiswa {
     private $jurusan;
 
     //fungsi untuk menginisialisasikan class mahasiswa
-    public function __construct(){
+    public function __construct()
+    {
         $this->nama = null;
         $this->nim = null;
         $this->jurusan = null;
     }
     //metode setter
     //fungsi untuk mengatur value dari atribut nama 
-    public function setNama($nama){
+    public function setNama($nama)
+    {
         $this->nama = $nama;
     }
     //fungsi untuk mengatur value dari inputan nim
-    public function setNim($nim){
+    public function setNim($nim)
+    {
         $this->nim = $nim;
     }
     //fungsi untuk mengatur value dari inputan jurusan
-    public function setJurusan($jurusan){
+    public function setJurusan($jurusan)
+    {
         $this->jurusan = $jurusan;
     }
     //metode getter
     //fungsi untuk mengambil value nama 
-    public function getNama(){
+    public function getNama()
+    {
         return $this->nama;
     }
     //fungsi untuk mengambil value nim
-    public function getNim(){
+    public function getNim()
+    {
         return $this->nim;
     }
     //fungsi untuk mengambil value jurusan
-    public function getJurusan(){
+    public function getJurusan()
+    {
         return $this->jurusan;
     }
 
     //fungsi untuk menampilkan data nama, nim, dan jurusan
-    public function tampilkanData(){
-        echo $this->nama.", " .$this->nim.", ".$this->jurusan;
+    public function tampilkanData()
+    {
+        echo $this->nama . ", " . $this->nim . ", " . $this->jurusan;
     }
 }
 
